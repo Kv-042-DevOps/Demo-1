9fa8369f8628cf1ae5f36d8a00364bb8dc6c0feb
sergiiglad <sergii.gladchenko@gmail.com>
2018-08-06 11:51:14 +0300

    Install packages for developer

--- shell/develop.sh
+++ shell/develop.sh
@@ -2,6 +2,9 @@
 #Code below will set up an DEVELOPER environment
 
 #git clone
+# Install packages for developer
+
+sudo rpm -ivh http://github.com/Kv-042-DevOps/Demo-1/raw/dev/rpmbuild/RPMS/noarch/demo1-dev-init-1-1.el7.centos.noarch.rpm
 
 #cd app directory
 
