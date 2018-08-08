87ca1199c1029a9e6106a1ca8a9c036dfee1c411
sergiiglad <sergii.gladchenko@gmail.com>
2018-08-08 13:26:32 +0300

    script

--- app/lab1/deploy-check.sh
+++ app/lab1/deploy-check.sh
@@ -1,4 +1,4 @@
-#/usb/bin/env bash
+#!/usb/bin/env bash
 #
 # This scropt wil prepeare file settings.py to deploy
 
@@ -24,6 +24,8 @@ source secret_key.txt
 
 if [ -n $SECRET_KEY ]; then
 	echo "SECRET_KEY SET SUCCESS"
+	echo  $SECRET_KEY > secret_key.txt   #save key to file
+	mv secret_key.txt ..
 	echo $SECRET_KEY
 else 
 	echo "SECRET_KEY NOT SET"
@@ -32,17 +34,17 @@ fi
 
 # new schema load SECRET_KEY save file $FILED
 
-sed -i "/SECRET_KEY/c\ \nif \"SECRET_KEY\" in os.environ:\n\t SECRET_KEY=os.environ['SECRET_KEY']\nelse:\n\twith open('/etc/secret_key.txt') as f:\n\tSECRET_KEY = f.read().strip()\n" $FILED
+sed -i "/SECRET_KEY/c\ \nif \"SECRET_KEY\" in os.environ:\n\t SECRET_KEY=os.environ['SECRET_KEY']\nelse:\n\twith open('secret_key.txt') as f:\n\t\tSECRET_KEY = f.read().strip()\n" $FILED
 
 # Change Debug mode
 
-sed -i "/DEBUG/c\DEBUG = False" $FILED
+sed -i "/^DEBUG/c\DEBUG = False" $FILED
 
-# set hostname
+# allowed site
 
 echo $HOSTNAME
 
-sed -i "/ALLOWED_HOSTS/c\ALLOWED_HOSTS = [$HOSTNAME]" $FILED
+sed -i "/ALLOWED_HOSTS/c\ALLOWED_HOSTS = [\'.cs50.io\']" $FILED
 
 
 
